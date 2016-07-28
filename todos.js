e0b46dff3b5d2949eadcd713609cd935c109c614
LikeJasper <williamclark.home@gmail.com>
2016-07-28 11:42:37 +0200

    define handleSubmit

--- todos.js
+++ todos.js
@@ -1,3 +1,7 @@
+const handleSubmit = () => {
+  console.log('Handling submit')
+}
+
 window.onload = () => {
   const formElement = document.getElementById("form")
   formElement.addEventListener("submit", handleSubmit)
